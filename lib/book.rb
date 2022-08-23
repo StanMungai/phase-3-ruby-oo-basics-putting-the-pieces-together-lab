b51a757a3d9d83adbb61bf18371eb479117abf06
stanford kangethe <stanfordkangethe16@gmail.com>
2022-08-23 06:31:05 +0300

    object orientation basics complete

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
+class Book
+  attr_reader :title, :author, :page_count, :genre
+  attr_writer :author, :page_count, :genre
 
+  def initialize(name)
+    @title = name
+  end
 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
+
+book = Book.new("Adventures of Huckleberry Finn")
+puts "Title: #{book.title}"
+
+book.author = "Mark Twain"
+puts "Author: #{book.author}" 
+
+book.page_count = 350
+puts "Pages: #{book.page_count}"
+
+book.genre = "Adventure"
+puts "Genre: #{book.genre}"
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,31 @@
 # Make your shoe class here!
+class Shoe 
+  attr_reader :brand, :color, :size, :material, :condition
+  attr_writer :color, :size, :material, :condition
+
+  def initialize(name)
+    @brand = name
+  end
+
+  def cobble
+    puts "Your shoe is as good as new!"
+    @condition = "new"
+  end
+
+end
+
+sneakers = Shoe.new("Timberland")
+puts "Brand: #{sneakers.brand}"
+
+sneakers.color = "Black"
+puts "Color: #{sneakers.color}"
+
+sneakers.size = 42
+puts "Size: #{sneakers.size}"
+
+sneakers.material = "leather"
+puts "Material: #{sneakers.material}"
+
+sneakers.condition = "new"
+puts "Condition: #{sneakers.condition}"
+
